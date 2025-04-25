b5200bc3747c19cece8029e08d4891d6370a6147
Rajat Singh <rs2382001@gmail.com>
2025-04-25 14:13:31 +0530

    Contain Duplicate

---
+++ easy/containsDuplicate.js
@@ -0,0 +1,66 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+
+// Output: true
+
+// Explanation:
+
+// The element 1 occurs at the indices 0 and 3.
+
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+
+// Output: false
+
+// Explanation:
+
+// All elements are distinct.
+
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+// Output: true
+
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+
+//solution
+
+
+
+// arr, not empty, always integer, can be negative numbers
+// boolean , if it containsDuplicate return a boolean. true or false
+// example:-/{
+// [1,2,3] => false;
+// [1,3,4,1] => true;
+// [1,1,1,1] => true;
+
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let seen = new Set()
+    for(let i = 0; i < nums.length; i++){
+      if(seen.has(nums[i])){
+        return true;
+      }
+      seen.add(nums[i])
+    }
+    return false;
+};
+
