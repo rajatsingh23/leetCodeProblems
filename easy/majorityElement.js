b9519d6b044232ce75cb64b3a1778734141f2700
Rajat Singh <rs2382001@gmail.com>
2025-04-26 12:23:34 +0530

    Majority Elements

---
+++ easy/majorityElement.js
@@ -0,0 +1,54 @@
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+ 
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 5 * 104
+// -109 <= nums[i] <= 109
+
+
+//solution
+
+
+// array, is not Empty, different, 
+// majority number (int)
+// [1,2,1,1] => 1 
+// [1] => 1 
+// [1,1,2,2, 2] => 2
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let freq = {}
+    for(let i = 0; i < nums.length; i++){
+      let key = nums[i].toString()
+      freq[key] = (freq[key] || 0) + 1
+    }
+    let maxkey = Object.keys(freq)[0];
+    let maxKeyValue = freq[maxkey]
+    for (const key in freq){
+      if(freq[key] > maxKeyValue){
+        maxKeyValue = freq[key];
+        maxkey = key 
+      }
+    }
+    return parseInt(maxkey) 
+};
