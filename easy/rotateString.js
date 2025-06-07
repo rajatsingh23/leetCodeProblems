5d88ce043a04b92c8b86ce61bba490fc6207bade
Rajat Singh <rs2382001@gmail.com>
2025-06-07 22:10:29 +0530

    Rotate String

---
+++ easy/rotateString.js
@@ -0,0 +1,46 @@
+// Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+// A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+// For example, if s = "abcde", then it will be "bcdea" after one shift.
+ 
+
+// Example 1:
+
+// Input: s = "abcde", goal = "cdeab"
+// Output: true
+// Example 2:
+
+// Input: s = "abcde", goal = "abced"
+// Output: false
+ 
+
+// Constraints:
+
+// 1 <= s.length, goal.length <= 100
+// s and goal consist of lowercase English letters.
+
+
+//solution
+
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function(s, goal) {
+    if(s.length !== goal.length){
+        return false;
+    }
+    let count = 0; 
+    let str = s.split('');
+    while(count < s.length){
+        let add = str.shift()
+        str.push(add)
+        if(str.join('') === goal){
+            return true;
+        }
+        count++;
+    }
+    return false;
+};
