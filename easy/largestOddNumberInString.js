6e36bbe0869d95ac96f47ccbbec42562bdda1449
Rajat Singh <rs2382001@gmail.com>
2025-08-11 23:34:06 +0530

    Larget Odd Number In String

---
+++ easy/largestOddNumberInString.js
@@ -0,0 +1,56 @@
+// 1903. Largest Odd Number in String
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
+
+// A substring is a contiguous sequence of characters within a string.
+
+ 
+
+// Example 1:
+
+// Input: num = "52"
+// Output: "5"
+// Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the only odd number.
+// Example 2:
+
+// Input: num = "4206"
+// Output: ""
+// Explanation: There are no odd numbers in "4206".
+// Example 3:
+
+// Input: num = "35427"
+// Output: "35427"
+// Explanation: "35427" is already an odd number.
+ 
+
+// Constraints:
+
+// 1 <= num.length <= 105
+// num only consists of digits and does not contain any leading zeros.
+
+//solution
+
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestOddNumber = function(num) {
+    //check the last number if it's odd just return the string, make an arr, loop through the arr, check if odd then add to temp if temp is greater than ans then ans = temp;
+
+    let arr = num.split('')
+
+    while(arr.length > 0){
+        if(Number(arr[arr.length - 1]) % 2 !== 0){
+            
+            break;
+        }
+        arr.pop();
+    }
+
+    return arr.join('');
+};
