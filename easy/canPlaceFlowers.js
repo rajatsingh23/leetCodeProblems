f53823ff40c5d27248f7355857f30715caca951c
Rajat Singh <rs2382001@gmail.com>
2025-08-20 19:58:49 +0530

    Can Place Flowers

---
+++ easy/canPlaceFlowers.js
@@ -0,0 +1,50 @@
+// 605. Can Place Flowers
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+// Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: flowerbed = [1,0,0,0,1], n = 1
+// Output: true
+// Example 2:
+
+// Input: flowerbed = [1,0,0,0,1], n = 2
+// Output: false
+ 
+
+// Constraints:
+
+// 1 <= flowerbed.length <= 2 * 104
+// flowerbed[i] is 0 or 1.
+// There are no two adjacent flowers in flowerbed.
+// 0 <= n <= flowerbed.length
+
+
+//solution
+
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    //loop through the arr, if previous and next is not 1 count++, if count <= n return true;
+    let count = 0;
+    for(let i = 0; i < flowerbed.length; i++){
+        if(flowerbed[i] === 0){
+            if(flowerbed[i-1] !== 1 && flowerbed[i+1] !== 1){
+                count++;
+                flowerbed[i] = 1
+            }
+        }
+    }
+    return count >= n;
+};
