ea42b7445ed88cb179ec4a3cb52a544c44c13f8f
Rajat Singh <rs2382001@gmail.com>
2025-09-15 20:42:37 +0530

    First Unique Character in a string

---
+++ easy/firstUniqueCharacterInAString.js
@@ -0,0 +1,60 @@
+// 387. First Unique Character in a String
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+ 
+
+// Example 1:
+
+// Input: s = "leetcode"
+
+// Output: 0
+
+// Explanation:
+
+// The character 'l' at index 0 is the first character that does not occur at any other index.
+
+// Example 2:
+
+// Input: s = "loveleetcode"
+
+// Output: 2
+
+// Example 3:
+
+// Input: s = "aabb"
+
+// Output: -1
+
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s consists of only lowercase English letters.
+
+//solution
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    let freq = {}
+
+    for(let i = 0; i < s.length; i++){
+        freq[s[i]] = (freq[s[i]] || 0) + 1
+    }
+
+    for(let i = 0; i < s.length; i++){
+        if(freq[s[i]] === 1){
+            return i
+        }
+    }
+
+    return -1
+    
+};
