e0e375edc5d06f9ff5df4df7bbc49f87eb56a2e8
Rajat Singh <rs2382001@gmail.com>
2026-01-04 20:52:45 +0530

    Maximum Number of Balls in a Box

---
+++ easy/MaximumNumberOfBallsInABox.js
@@ -0,0 +1,74 @@
+// 1742. Maximum Number of Balls in a Box
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are working in a ball factory where you have n balls numbered from lowLimit up to highLimit inclusive (i.e., n == highLimit - lowLimit + 1), and an infinite number of boxes numbered from 1 to infinity.
+
+// Your job at this factory is to put each ball in the box with a number equal to the sum of digits of the ball's number. For example, the ball number 321 will be put in the box number 3 + 2 + 1 = 6 and the ball number 10 will be put in the box number 1 + 0 = 1.
+
+// Given two integers lowLimit and highLimit, return the number of balls in the box with the most balls.
+
+ 
+
+// Example 1:
+
+// Input: lowLimit = 1, highLimit = 10
+// Output: 2
+// Explanation:
+// Box Number:  1 2 3 4 5 6 7 8 9 10 11 ...
+// Ball Count:  2 1 1 1 1 1 1 1 1 0  0  ...
+// Box 1 has the most number of balls with 2 balls.
+// Example 2:
+
+// Input: lowLimit = 5, highLimit = 15
+// Output: 2
+// Explanation:
+// Box Number:  1 2 3 4 5 6 7 8 9 10 11 ...
+// Ball Count:  1 1 1 1 2 2 1 1 1 0  0  ...
+// Boxes 5 and 6 have the most number of balls with 2 balls in each.
+// Example 3:
+
+// Input: lowLimit = 19, highLimit = 28
+// Output: 2
+// Explanation:
+// Box Number:  1 2 3 4 5 6 7 8 9 10 11 12 ...
+// Ball Count:  0 1 1 1 1 1 1 1 1 2  0  0  ...
+// Box 10 has the most number of balls with 2 balls.
+ 
+
+// Constraints:
+
+// 1 <= lowLimit <= highLimit <= 105
+
+//solution
+
+/**
+ * @param {number} lowLimit
+ * @param {number} highLimit
+ * @return {number}
+ */
+var countBalls = function(lowlimit, highlimit) {
+    //make an map;
+    //loop between lowlimit and highlimit;
+    //make an temp inside lowlimit;
+    //while it is more than 0 keep adding it;
+    //incremenet inside the map;
+    //loop through the map return with highest value;
+    let map = new Map()
+    let ans = 0;
+    for(let i = lowlimit; i <= highlimit; i++){
+        let temp = i;
+        let box = 0;
+        while(temp > 0){
+            box = box + temp % 10
+            temp = Math.floor(temp / 10)
+        }
+        map.set(box, (map.get(box) || 0) + 1)
+        ans = Math.max(ans, map.get(box))
+    }
+
+    return ans;
+};
