80f3785eca99be56e5c2d2ba7c4b6bcef854906e
Rajat Singh <rs2382001@gmail.com>
2026-01-14 18:56:24 +0530

    Sliding window ans

--- medium/countCompleteSubarraysInAnArr.js
+++ medium/countCompleteSubarraysInAnArr.js
@@ -1,40 +1,3 @@
-// 2799. Count Complete Subarrays in an Array
-// Solved
-// Medium
-// Topics
-// premium lock icon
-// Companies
-// Hint
-// You are given an array nums consisting of positive integers.
-
-// We call a subarray of an array complete if the following condition is satisfied:
-
-// The number of distinct elements in the subarray is equal to the number of distinct elements in the whole array.
-// Return the number of complete subarrays.
-
-// A subarray is a contiguous non-empty part of an array.
-
- 
-
-// Example 1:
-
-// Input: nums = [1,3,1,2,2]
-// Output: 4
-// Explanation: The complete subarrays are the following: [1,3,1,2], [1,3,1,2,2], [3,1,2] and [3,1,2,2].
-// Example 2:
-
-// Input: nums = [5,5,5,5]
-// Output: 10
-// Explanation: The array consists only of the integer 5, so any subarray is complete. The number of subarrays that we can choose is 10.
- 
-
-// Constraints:
-
-// 1 <= nums.length <= 1000
-// 1 <= nums[i] <= 2000
-
-//solution
-
 /**
  * @param {number[]} nums
  * @return {number}
@@ -46,15 +9,41 @@ var countCompleteSubarrays = function(nums) {
     //loop through nums using left;
     //if set size is 0 windowAns++;
     //if loop end move forward;
+
+
+    //bruteforce;
+    // let windowAns = 0;
+    // let set = new Set(nums)
+    // for(let l = 0; l < nums.length; l++){
+    //     let temp = new Set();
+    //     if(set.size === 1){
+    //         return (nums.length *(nums.length + 1)) / 2
+    //     }
+    //     for(let r = l; r < nums.length; r++){
+    //         temp.add(nums[r]);
+    //         if(temp.size === set.size){
+    //             windowAns++;
+    //         }
+    //     }
+    // }
+
+
+    //optimised;
     let windowAns = 0;
-    let set = new Set(nums)
-    for(let l = 0; l < nums.length; l++){
-        let temp = new Set();
-        for(let r = l; r < nums.length; r++){
-            temp.add(nums[r]);
-            if(temp.size === set.size){
-                windowAns++;
-            }
+    let totalDistinct = new Set(nums).size;
+    let left = 0;
+    let distinct = 0;
+    let map = new Map();
+
+    for(let i = 0; i < nums.length; i++){
+        map.set(nums[i], (map.get(nums[i]) || 0) + 1);
+        if(map.get(nums[i]) === 1) distinct++;
+
+        while(distinct === totalDistinct){
+            windowAns += nums.length - i;
+            map.set(nums[left], map.get(nums[left]) - 1);
+            if(map.get(nums[left]) === 0) distinct--;
+            left++;
         }
     }
 
