0e1b4c274cb776c920ccf17146793727d73ca58c
Rajat Singh <rs2382001@gmail.com>
2026-02-09 20:02:03 +0530

    Reverse String Prefix

---
+++ easy/reverseStringPrefix.js
@@ -0,0 +1,63 @@
+// 3794. Reverse String Prefix
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given a string s and an integer k.
+
+// Reverse the first k characters of s and return the resulting string.
+
+ 
+
+// Example 1:
+
+// Input: s = "abcd", k = 2
+
+// Output: "bacd"
+
+// Explanation:​​​​​​​
+
+// The first k = 2 characters "ab" are reversed to "ba". The final resulting string is "bacd".
+
+// Example 2:
+
+// Input: s = "xyz", k = 3
+
+// Output: "zyx"
+
+// Explanation:
+
+// The first k = 3 characters "xyz" are reversed to "zyx". The final resulting string is "zyx".
+
+// Example 3:
+
+// Input: s = "hey", k = 1
+
+// Output: "hey"
+
+// Explanation:
+
+// The first k = 1 character "h" remains unchanged on reversal. The final resulting string is "hey".
+
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 100
+// s consists of lowercase English letters.
+// 1 <= k <= s.length
+
+//solution
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reversePrefix = function(s, k) {
+    let lastString = s.slice(k, s.length);
+    let prefix = s.slice(0,k).split('').reverse().join('')
+    return prefix + lastString;
+};
