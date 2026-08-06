f64fd8ea8f48cfb9758d2c212ec8f9d4c71db0f6
Rajat Singh <rs2382001@gmail.com>
2026-08-06 20:46:04 +0530

    Delete Nodes From Linked List Present in Arr

---
+++ medium/deleteNodesFromLinkedListPresentInArr.js
@@ -0,0 +1,95 @@
+// 3217. Delete Nodes From Linked List Present in Array
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given an array of integers nums and the head of a linked list. Return the head of the modified linked list after removing all nodes from the linked list that have a value that exists in nums.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3], head = [1,2,3,4,5]
+
+// Output: [4,5]
+
+// Explanation:
+
+
+
+// Remove the nodes with values 1, 2, and 3.
+
+// Example 2:
+
+// Input: nums = [1], head = [1,2,1,2,1,2]
+
+// Output: [2,2,2]
+
+// Explanation:
+
+
+
+// Remove the nodes with value 1.
+
+// Example 3:
+
+// Input: nums = [5], head = [1,2,3,4]
+
+// Output: [1,2,3,4]
+
+// Explanation:
+
+
+
+// No node has value 5.
+
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 105
+// All elements in nums are unique.
+// The number of nodes in the given list is in the range [1, 105].
+// 1 <= Node.val <= 105
+// The input is generated such that there is at least one node in the linked list that has a value not present in nums.
+
+//solution
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var modifiedList = function(nums, head) {
+    //make set of arr;
+    //if set has val then remove that val;
+    //return head;
+    let dummy = new ListNode(-1);
+    let prev = dummy;
+    let set = new Set();
+    for(let num of nums){
+        set.add(num)
+    }
+    let current = head;
+    while(current){
+        let val = current.val;
+        if(!set.has(val)){
+            prev.next = current;
+            prev = prev.next;
+        }else{
+            prev.next = current.next;
+        }
+        current = current.next;
+    }
+    return dummy.next;
+};
