0b71df7a665836d8a64375ba88aa5a969feef1f4
Rajat Singh <rs2382001@gmail.com>
2026-08-17 18:46:34 +0530

    Odd Even Linked List - Optimized

--- medium/oddEvenLinkedList.js
+++ medium/oddEvenLinkedList.js
@@ -70,3 +70,19 @@ var oddEvenList = function(head) {
     oddDummyTail.next = dummy;
     return oddDummy.next;
 };
+
+//optimized
+
+    if(!head || !head.next) return head;
+    let odd = head;
+    let even = head.next;
+    let evenHead = even;
+    while(even && even.next){
+        odd.next = even.next;
+        odd = odd.next;
+
+        even.next = odd.next;
+        even = even.next;
+    }
+    odd.next = evenHead;
+    return head;
