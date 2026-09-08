ad3a17c90ce0ed9cb748605c74ab35dce767a117
Rajat Singh <rs2382001@gmail.com>
2026-09-08 21:29:00 +0530

    check good integer

---
+++ easy/checkGoodInteger.js
@@ -0,0 +1,66 @@
+// 3959. Check Good Integer
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// You are given a positive integer n.
+
+// Let digitSum be the sum of the digits of n, and let squareSum be the sum of the squares of the digits of n.
+
+// An integer is called good if squareSum - digitSum >= 50.
+
+// Return true if n is good. Otherwise, return false.
+
+ 
+
+// Example 1:
+
+// Input: n = 1000
+
+// Output: false
+
+// Explanation:
+
+// The digits of 1000 are 1, 0, 0, and 0.
+// The digitSum is 1 + 0 + 0 + 0 = 1.
+// The squareSum is 12 + 02 + 02 + 02 = 1.
+// The squareSum - digitSum is 1 - 1 = 0. As 0 is not greater than or equal to 50, the output is false.
+// Example 2:
+
+// Input: n = 19
+
+// Output: true
+
+// Explanation:
+
+// The digits of 19 are 1 and 9.
+// The digitSum is 1 + 9 = 10.
+// The squareSum is 12 + 92 = 1 + 81 = 82.
+// The squareSum - digitSum is 82 - 10 = 72. As 72 is greater than or equal to 50, the output is true.
+ 
+
+// Constraints:
+
+// 1 <= n <= 109
+ 
+
+
+//solution
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var checkGoodInteger = function(n) {
+    let sum = 0;
+    let square = 0;
+    while(n > 0){
+        let last = n % 10;
+        sum += last;
+        square += last ** 2;
+        n = Math.floor(n/10)
+    }
+    return square - sum >= 50;
+};
